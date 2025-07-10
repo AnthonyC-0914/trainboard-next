1345841187cc3e557672993e888c15c3f8859be0
theajs87 <117082737+theajs87@users.noreply.github.com>
2025-07-10 11:47:55 +0100

    Update src/apiFetchFunctions/getUrlBase.tsx
    Co-authored-by: Maurice Byrne <107932418+MauByr@users.noreply.github.com>

--- src/apiFetchFunctions/getUrlBase.tsx
+++ src/apiFetchFunctions/getUrlBase.tsx
@@ -1,6 +1,7 @@
 export function getUrlBase(): string {
     const urlBase: string | undefined = process.env["urlBase"]
-    if (urlBase === undefined) {
+    if (!urlBase) {
+
         throw new Error("urlBase is undefined");
     }
     return urlBase;
